d63f65402630f65d7a5a668c55a1277097d69266
ayyoub <42214729+soufiayyoub@users.noreply.github.com>
2023-10-15 17:39:22 +0100

    Create pipe.groovy

---
+++ src/pipe.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any // You can specify a specific agent based on your environment
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Check out the source code from your Git repository
+                git url: 'hhttps://github.com/soufiayyoub/hello-world.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                // Build the Spring Boot application
+                sh 'mvn clean install'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                // Run tests if any
+                sh 'mvn test'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                // Deploy the Spring Boot application
+                sh 'java -jar target/your-app-name.jar &'
+            }
+        }
+    }
+
+    // post {
+    //     success {
+    //         // You can add post-build actions here
+    //     }
+    // }
+}
