60b63044fd0b135ca87c72be03dd011bc4a71509
ayyoub <42214729+soufiayyoub@users.noreply.github.com>
2023-10-15 18:43:32 +0100

    Update pipe.groovy

--- src/pipe.groovy
+++ src/pipe.groovy
@@ -1,4 +1,6 @@
 node {
+    
+    def mvnHome = tool name: 'maven', type: 'maven'
     // agent any // You can specify a specific agent based on your environment
     stage('Checkout') {
             // Check out the source code from your Git repository
@@ -7,7 +9,6 @@ node {
     }
 
     stage('Build') {
-        def mvnHome = tool name: 'maven', type: 'maven'
         sh "${mvnHome}/bin/mvn -version"
         // Build the Spring Boot application
         sh "${mvnHome}/bin/mvn clean install"
