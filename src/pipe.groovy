726dbe48ba08f783330c8ac627dc82ce28d679df
ayyoub <42214729+soufiayyoub@users.noreply.github.com>
2023-10-15 18:01:05 +0100

    Update pipe.groovy

--- src/pipe.groovy
+++ src/pipe.groovy
@@ -1,34 +1,32 @@
-pipeline {
-    agent any // You can specify a specific agent based on your environment
+node {
+    // agent any // You can specify a specific agent based on your environment
 
-    stages {
-        stage('Checkout') {
-            steps {
-                // Check out the source code from your Git repository
-                // git url: 'hhttps://github.com/soufiayyoub/hello-world.git'
-                checkout scm
-            }
+    stage('Checkout') {
+        steps {
+            // Check out the source code from your Git repository
+            // git url: 'hhttps://github.com/soufiayyoub/hello-world.git'
+            checkout scm
         }
+    }
 
-        stage('Build') {
-            steps {
-                // Build the Spring Boot application
-                sh 'mvn clean install'
-            }
+    stage('Build') {
+        steps {
+            // Build the Spring Boot application
+            sh 'mvn clean install'
         }
+    }
 
-        stage('Test') {
-            steps {
-                // Run tests if any
-                sh 'mvn test'
-            }
+    stage('Test') {
+        steps {
+            // Run tests if any
+            sh 'mvn test'
         }
+    }
 
-        stage('Deploy') {
-            steps {
-                // Deploy the Spring Boot application
-                sh 'java -jar target/your-app-name.jar &'
-            }
+    stage('Deploy') {
+        steps {
+            // Deploy the Spring Boot application
+            sh 'java -jar target/your-app-name.jar &'
         }
     }
 
